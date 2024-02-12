0079968a68c910c347a449b41b5706d9a39e05c5
Valerii <vd.alternative@gmail.com>
2024-02-12 23:18:57 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Circles } from 'react-loader-spinner';
+import { CirclesWithBar } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <Circles
-      height="80"
-      width="80"
-      color="#0a1897"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
+    <CirclesWithBar
+    height="100"
+    width="100"
+    color="blue"
+    outerCircleColor="yellow"
+    innerCircleColor="blue"
+    barColor="blue"
+    ariaLabel="circles-with-bar-loading"
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
     />
   );
 };
